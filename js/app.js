af44f668b1d7e2017d7510eebb1ef23ca54ebdad
EstrellaBF <stern.von.nord>
2018-02-07 22:00:46 -0500

    cambiando index a collage html

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ document.addEventListener('drop', drop);
 function createImage(images) {
   for (let i = 0; i < images.length; i++) {
     var image = document.createElement('img');
-    image.setAttribute('src', 'assets/images/' + images[i]['name']);
+    image.setAttribute('src', '../assets/images/' + images[i]['name']);
     image.setAttribute("id", images[i]['id']);
     image.classList.add('img-thumbnail', 'image-small');
     containerImages.appendChild(image);
